9f56c50b5da55e74def727a47505985f1a1b3f3b
domefederico <domefederico@gmail.com>
2024-05-20 11:20:17 -0300

    SBTtest subido

--- test/SBTtest.java
+++ test/SBTtest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import uy.edu.um.prog2.adt.binarytree.*;
 
@@ -5,26 +6,61 @@ import static org.junit.Assert.*;
 
 public class SBTtest {
 
-    public MySearchBinaryTree<Integer, Integer> sbt = new MySearchBinaryTreeImpl<>();
+    public MySearchBinaryTree<Integer, Integer> sbt = null;
 
-    @Test
-    public void addTest() {
+    @Before
+    public void initsbt() {
+        sbt = new MySearchBinaryTreeImpl<>();
         sbt.add(4,25);
         sbt.add(2,8);
         sbt.add(6,33);
         sbt.add(3,50);
         sbt.add(1,13);
         sbt.add(5,24);
+    }
+
+    @Test
+    public void addTest() {
         int s = sbt.getRoot().getKey();
         int d = sbt.getRoot().getLeft().getKey();
-        int r = sbt.getRoot().getRight().getLeft().getKey();
+        int r = sbt.getRoot().getRight().getLeft().getValue();
         assertEquals(s,4);
+        assertEquals(d,2);
+        assertEquals(r,24);
+    }
 
+    @Test
+    public void findTest() {
+        int s = sbt.find(3);
+        int d = sbt.find(4);
+        assertEquals(s,50);
+        assertEquals(d,25);
+        assertEquals(sbt.find(8),null);
     }
 
 
+    @Test
+    public void containsTest() {
+        sbt.add(4,25);
+        sbt.add(2,8);
+        sbt.add(6,33);
+        sbt.add(3,50);
+        sbt.add(1,13);
+        sbt.add(5,24);
+        assertTrue(sbt.contains(2));
+        assertFalse(sbt.contains(8));
+    }
 
 
-
+    @Test
+    public void removeTest() {
+        sbt.add(4,25);
+        sbt.add(2,8);
+        sbt.add(6,33);
+        sbt.add(3,50);
+        sbt.add(1,13);
+        sbt.add(5,24);
+        assertTrue(sbt.contains(2));
+    }
 
 }
